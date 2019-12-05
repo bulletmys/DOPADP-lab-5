8d1e84e3ab0fde345e9f1d7d8a7f9505d3fd3a2e
bulletmys <bulletmys@yandex.ru>
2019-12-06 01:04:22 +0300

    Scripted auto-commit on change (2019-12-06 01:04:22) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -24,7 +24,7 @@ public class HttpClient {
 
     private ActorRef cacheActor;
     private Duration duration = Duration.ofSeconds(5);
-    private Sink<>
+    private Sink<Pair<String, Integer>, >
 
     HttpClient(ActorSystem system) {
         cacheActor = system.actorOf(CacheActor.props(), "cacheActor");
