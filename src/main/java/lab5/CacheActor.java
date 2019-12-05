f43997162753c2fc5c5427682161a6c5cd3d0c1c
bulletmys <bulletmys@yandex.ru>
2019-12-05 18:40:52 +0300

    Scripted auto-commit on change (2019-12-05 18:40:52) by gitwatch.sh

--- src/main/java/lab5/CacheActor.java
+++ src/main/java/lab5/CacheActor.java
@@ -1,4 +1,11 @@
 package lab5;
 
-public class CacheActor {
+import akka.actor.AbstractActor;
+
+public class CacheActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
