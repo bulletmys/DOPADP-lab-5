b36353176b41ae871bba74bf0f246c432780361a
bulletmys <bulletmys@yandex.ru>
2019-12-05 19:24:38 +0300

    Scripted auto-commit on change (2019-12-05 19:24:38) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -3,6 +3,7 @@ package lab5;
 import akka.NotUsed;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import javafx.util.Pair;
 
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -11,7 +12,7 @@ public class HttpClient {
 
     Flow<HttpRequest, HttpResponse, NotUsed> httpFlow(ActorMaterializer materializer) {
         return Flow.of(HttpRequest.class)
-                .map()
-                .mapAsync()
+                .map(k -> new Pair<>())
+//                .mapAsync()
     }
 }
