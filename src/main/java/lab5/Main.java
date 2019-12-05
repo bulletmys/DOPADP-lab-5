9b862bc9496bc62ea2038efebd11722cd7d4ac3c
bulletmys <bulletmys@yandex.ru>
2019-12-05 18:26:58 +0300

    Scripted auto-commit on change (2019-12-05 18:26:58) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -18,8 +18,10 @@ public class Main {
         final Http http = Http.get(system);
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
-        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <
+//        вызов
+//        метода которому передаем Http, ActorSystem и ActorMaterializer
+        >;
         final CompletionStage<Ser
         verBinding> binding = http.bindAndHandle(
                 routeFlow,
