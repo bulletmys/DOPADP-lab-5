0c034b8743e78673046498ac4f031724d09b10cb
bulletmys <bulletmys@yandex.ru>
2019-12-05 19:35:58 +0300

    Scripted auto-commit on change (2019-12-05 19:35:58) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -12,10 +12,10 @@ public class HttpClient {
 
     Flow<HttpRequest, HttpResponse, NotUsed> httpFlow(ActorMaterializer materializer) {
         return Flow.of(HttpRequest.class)
-                .map(request -> new Pair<String, Integer>(
-                        request.getUri().query().getOrElse("testURL", ""),
-                        Integer.parseInt(request.getUri().query().getOrElse("count", ""))
-                        ))
-//                .mapAsync()
+                .map(request -> {
+                    return new Pair<String, Integer>(
+                            request.getUri().query().getOrElse("testURL", ""),
+                            Integer.parseInt(request.getUri().query().getOrElse("count", "")));
+                });
     }
 }
