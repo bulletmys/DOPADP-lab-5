9b11acfec01243d1a057ff95fda5eb57da6a9e82
bulletmys <bulletmys@yandex.ru>
2019-12-05 19:01:34 +0300

    Scripted auto-commit on change (2019-12-05 19:01:34) by gitwatch.sh

--- src/main/java/lab5/CacheActor.java
+++ src/main/java/lab5/CacheActor.java
@@ -1,12 +1,32 @@
 package lab5;
 
 import akka.actor.AbstractActor;
+import akka.actor.Props;
+
+import java.util.HashMap;
 
 public class CacheActor extends AbstractActor {
+
+    private HashMap<String, Integer> hashRes = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match()
+                .match(TestRequest.class, mail -> {
+                    if (hashRes.containsKey(mail.getHostName())) {
+                        sender().tell(new TestResponse(mail.getHostName(), hashRes.get(mail.getHostName())), self());
+                    } else {
+                        sender().tell("NotFound", self());
+                    }
+                })
+                .match(TestResponse.class, mail -> {
+                    hashRes.put(mail.getHostName(), mail.getAverageTime());
+                })
+                .build();
     }
+
+    public static Props props() {
+        return Props.create(TestKeeperActor.class);
+    }
+
 }
--- src/main/java/lab5/TestRequest.java
+++ src/main/java/lab5/TestRequest.java
@@ -1,4 +1,17 @@
 package lab5;
 
 public class TestRequest {
+    private String hostName;
+
+    TestRequest(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
 }
