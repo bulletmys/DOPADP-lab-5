e5b249e34692fd9298429295791c79b6e67a7813
bulletmys <bulletmys@yandex.ru>
2019-12-06 00:52:12 +0300

    Scripted auto-commit on change (2019-12-06 00:52:12) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -42,8 +42,8 @@ public class HttpClient {
                                     }
                                 }))
                 .map(value -> {
-
                     cacheActor.tell(value, ActorRef.noSender());
+                    return 
                 })
     });
 }
