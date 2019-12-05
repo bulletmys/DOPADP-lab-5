222a08b0875c63dc3f1239d50bc04fa95a5d50d5
bulletmys <bulletmys@yandex.ru>
2019-12-05 18:48:11 +0300

    Scripted auto-commit on change (2019-12-05 18:48:11) by gitwatch.sh

--- src/main/java/lab5/TestResponse.java
+++ src/main/java/lab5/TestResponse.java
@@ -1,5 +1,27 @@
 package lab5;
 
 public class TestResponse {
-    private 
+    private String hostName;
+    private int averageTime;
+
+    TestResponse(String hostName, int averageTime) {
+        this.hostName = hostName;
+        this.averageTime = averageTime;
+    }
+
+    public void setAverageTime(int averageTime) {
+        this.averageTime = averageTime;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public int getAverageTime() {
+        return averageTime;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
 }
